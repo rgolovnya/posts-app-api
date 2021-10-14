1144da20849bcbc55ffeb5ca13cbee2a33083ccb
rgolovnya <rgolovnya@yahoo.com>
2021-10-14 13:08:10 +0100

    upvoting posts

--- app/posts/serializers.py
+++ app/posts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Vote
 
 class PostSerializer(serializers.ModelSerializer):
     poster = serializers.ReadOnlyField(source='poster.username')
@@ -9,3 +9,7 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = ['id', 'title', 'url', 'poster', 'poster_id', 'created']
 
+class VoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vote
+        fields = ['id']
